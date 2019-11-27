749d1635bbcde76f26a60475a33d2ed448166977
Bento <lbento525@gmail.com>
2019-11-27 21:23:05 +0000

    update our menu itens to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,11 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {/* {this.state.sections.map(({ title, imageUrl, id, size , linkUrl }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
+        ))} we can use the spread operator to pass this props this is equivalent */}
+        {this.state.sections.map(({ id, ...otherSections }) => (
+          <MenuItem key={id} {...otherSections} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
 //to destructuring props we use {} this way we destruct like title from props
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -18,4 +23,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
