59febc6dc135db24d2eeac4b38f7fff00b5fcfc7
Bento <lbento525@gmail.com>
2019-12-19 18:21:53 +0000

    (refers to the last 2 commits to) moved our directory and shop data into its own redicer, created respective selectores and update corresponding components with new redux flow for directory / shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-const ShopPage = ({ collections }) => {
+import { Route } from "react-router-dom";
+
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({ match }) => {
+  //console.log(match);
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,12 @@
 import { createSelector } from "reselect";
+//old when using shop data as array
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5
+// };
 
 const selectShop = state => state.shop;
 
@@ -6,3 +14,22 @@ export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+//because of the shop data modification now our collection overview and other component think it still is an array so we need to convert it
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
+
+//selector when shopData was an array, the problem of using data as array is that using find() on big arrays will take to long not good for performance so its better to use an object see hash tables
+// export const selectCollection = collectionUrlParam =>
+//   createSelector([selectCollections], collections =>
+//     collections.find(
+//       collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   );
